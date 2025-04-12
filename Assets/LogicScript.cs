57601baf0609fa2a3b687f947be231309b5f31b9
jmoneill15 <jmoneill28@gmail.com>
2025-04-12 02:02:50 -0400

    making some small fixes

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class LogicScript : MonoBehaviour
 {
     public int totalVehicles; //goal for player to make
+    public string vehicleType; 
 
     public string objective;
 
@@ -24,4 +26,6 @@ public class LogicScript : MonoBehaviour
     {
         
     }
+
+    public string[] vehicles;
 }
